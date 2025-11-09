e096ba2d7a17c2a9bf438398f9dc6f1690b1ef18
basurakid <92126346+basurakid@users.noreply.github.com>
2025-11-09 21:46:00 +0100

    add qr library, test it works with tesseract

--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
 import "./style.css";
 import { createWorker } from 'tesseract.js';
+import QRCode from 'qrcode';
 
-const cameraBtn = document.querySelector(".cameraBtn")
+const cameraBtn = document.querySelector(".cameraBtn");
+
+cameraBtn.addEventListener("click",()=>{getMedia({video:true, audio:false})})
 
 async function getMedia(constraints) {
+  console.log("We are so in!");
   let stream = null;
-try {
-    stream = await navigator.mediaDevices.getUserMedia(constraints);
-    /* use the stream */
+  try {
+      stream = await navigator.mediaDevices.getUserMedia(constraints);
+      /* use the stream */
   } catch (err) {
-    /* handle the error */
+      /* handle the error */
+      console.log(err);
   }
 }
 
 (async () => {
 
   const worker = await createWorker('eng');
-  const ret = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+  const testImg = document.querySelector("#test");
+  const ret = await worker.recognize(testImg);
+  const canvas = document.querySelector("#qrCanvas");
   console.log(ret.data.text);
+  QRCode.toCanvas(canvas,ret.data.text, (error)=>{
+    if (error) 
+      console.error(error);
+  })
   await worker.terminate();
 })();
 
